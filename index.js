072acaecb73c5e02d838cab69691dc5bea8c2807
Paris Val Baker <=>
2019-09-24 16:18:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,239 @@
+"use strict";
+
+var hashtable = require('alib-hashtable');
+const filterFuncs = hashtable('name');
+
+//filter funcs packaged functions,
+//////////////////////////////////////////////////////////////////////////////////////////
+function filter_eq(a, b) {
+  if (a === b) {
+    return true;
+  }
+  return false;
+}
+function filter_gt(a, b) {
+  if (a > b) {
+    return true;
+  }
+  return false;
+}
+function filter_lt(a, b) {
+  if (a < b) {
+    return true;
+  }
+  return false;
+}
+function filter_contains(a, b, options) {
+  if (options && options.ignoreCase) {
+    if (a.toLowerCase().includes(b.toLowerCase())) {
+      return true;
+    }
+  }
+  else {
+    if (a.includes(b)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//push filter funcs packaged functions, these are extensible through registerFilter
+//////////////////////////////////////////////////////////////////////////////////////////
+filterFuncs.set({
+  name: 'eq',
+  func: filter_eq
+});
+filterFuncs.set({
+  name: 'gt',
+  func: filter_gt
+});
+filterFuncs.set({
+  name: 'lt',
+  func: filter_lt
+});
+filterFuncs.set({
+  name: 'contains',
+  func: filter_contains
+});
+
+//chainable array filter library
+module.exports = function alibFilter(arrayToFilter) {
+  //filter stack
+  var filterStack = [];
+  var limitVal = null;
+  var offsetVal = 0;
+  var orderBySet = false;
+  var orderByComparator = null;
+  var orderByProp = null;
+
+  var registerFilter = function (name, func) {
+    filterFuncs.set({
+      name: name,
+      func: func
+    });
+  }
+
+  //add a filter
+  //////////////////////////////////////////////////////////////////////////////////////////
+  var filter = function (prop, name, value, options) {
+    filterStack.push({
+      prop: prop,
+      name: name,
+      value: value,
+      options: options
+    });
+
+    return this;
+  };
+
+  //limit & offset
+  //////////////////////////////////////////////////////////////////////////////////////////  
+  var limit = function (limit) {
+    limitVal = limit;
+
+    return this;
+  }
+  var offset = function (offset) {
+    offsetVal = offset;
+
+    return this;
+  }
+
+  var orderBy = function (prop, direction, type) {
+    orderBySet = true;
+    orderByProp = prop;
+
+    //ordering functions
+    var comp_string_desc = function (a, b) {
+      if (typeof a !== 'string') a = String(a);
+      if (typeof b !== 'string') b = String(b);
+      return a > b ? 1 : (a < b ? -1 : 0);
+    };
+    var comp_string_asc = function (a, b) {
+      if (typeof a !== 'string') a = String(a);
+      if (typeof b !== 'string') b = String(b);
+      return a > b ? -1 : (a < b ? 1 : 0);
+    };
+
+    if (direction === 'desc') {
+      orderByComparator = comp_string_desc;
+    }
+    if (direction === 'asc') {
+      orderByComparator = comp_string_asc;
+    }
+
+    return this;
+  }
+
+  function insertSorted(arr, item) {
+    // get the index we need to insert the item at
+    var min = 0;
+    var max = arr.length;
+    var index = Math.floor((min + max) / 2);
+    while (max > min) {
+      if (orderByProp !== null) {
+        if (orderByComparator(item[orderByProp], arr[index][orderByProp]) < 0) {
+          max = index;
+        } else {
+          min = index + 1;
+        }
+      }
+      else {
+        if (orderByComparator(item, arr[index]) < 0) {
+          max = index;
+        } else {
+          min = index + 1;
+        }
+      }
+
+      index = Math.floor((min + max) / 2);
+    }
+
+    // insert the item
+    arr.splice(index, 0, item);
+  };
+
+  //result passback, used at end of chain
+  var run = function () {
+    var offset = 0;
+    var limit = 0;
+    var results = [];
+    var resultsOrderBy = [];
+
+    //temp vars
+    var tfunc = null;
+    var tpropval = null;
+    var toptions = null;
+    var hasFailed = false;
+
+    //execute filter
+    for (var i = 0; i < arrayToFilter.length; i++) {
+      //check filters
+      hasFailed = false;
+      for (var j = 0; j < filterStack.length; j++) {
+        //check if prop present
+        if (arrayToFilter[i][filterStack[j].prop] || filterStack[j].prop === null) {
+          //get value
+          tpropval = ((filterStack[j].prop !== null) ? arrayToFilter[i][filterStack[j].prop] : arrayToFilter[i]);
+          toptions = null;
+          if (filterStack[j].options) {
+            toptions = filterStack[j].options;
+          }
+          //check
+          if (!filterFuncs.get(filterStack[j].name).func(tpropval, filterStack[j].value, toptions)) {
+            //failed this filter
+            hasFailed = true;
+            //break no need to check any more filters as failed this one
+            break;
+          }
+        }
+      }
+
+      //check limit / offset
+      if (!hasFailed) {
+        if (!orderBySet) {
+          if (offset >= offsetVal) {
+            if ((limitVal === null) || (results.length < limitVal)) {
+              //within limit and offset - push to output
+              results.push(arrayToFilter[i]);
+            }
+            else {
+              //exceeded limit
+              break;
+            }
+          }
+          //inc
+          offset++;
+        }
+        else {
+          //order by has been set so we need to do a full scan before running limit and offset
+          insertSorted(resultsOrderBy, arrayToFilter[i]);
+        }
+      }
+
+    }
+
+    if (!orderBySet) {
+      //return non ordered results
+      return results;
+    }
+
+    if (limitVal !== null) {
+      //return resultsOrderBy with applied offset / limit
+      return resultsOrderBy.slice(offsetVal, (limitVal + offsetVal));
+    }
+
+    return resultsOrderBy;
+  };
+
+  //return
+  return {
+    filter: filter,
+    limit: limit,
+    offset: offset,
+    orderBy: orderBy,
+    registerFilter: registerFilter,
+    run: run
+  };
+
+};
---
+++ test.js
@@ -0,0 +1,228 @@
+'use strict';
+
+/* deps:mocha */
+var assert = require('assert');
+
+//init
+var aquery = require('./');
+
+//vars
+var data = [
+    { color: 'blue', size: 44 },
+    { color: 'green', size: 12 },
+    { color: 'red', size: 18 },
+    { color: 'blue', size: 9 },
+    { color: 'blue', size: 4 },
+    { color: 'blue', size: 12 },
+];
+
+//data size + 10
+var datat1 = [
+    { color: 'blue', size: 44 },
+    { color: 'green', size: 12 },
+    { color: 'red', size: 18 },
+    { color: 'blue', size: 12 },
+];
+
+//data size + 10 and blue
+var datat2 = [
+    { color: 'blue', size: 44 },
+    { color: 'blue', size: 12 },
+];
+
+var data2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var data2t1 = [6, 7, 8, 9, 10, 6, 7, 8, 9, 10];
+var data2t2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var data2t3 = [4, 5, 6, 7, 8, 9, 10, 1, 2, 3];
+
+var data3 = [
+    { color: 'blue', size: 44 },
+    { color: 'green', size: 12 },
+    { color: 'red', size: 18 },
+    { color: 'blue', size: 9 },
+    { color: 'blue', size: 4 },
+    { color: 'blue', size: 12 },
+    { color: 'black', size: 12 },
+];
+var data3t1 = [
+    { color: 'blue', size: 44 },
+    { color: 'blue', size: 9 },
+    { color: 'blue', size: 4 },
+    { color: 'blue', size: 12 },
+    { color: 'black', size: 12 },
+];
+var data3t2 = [
+    { color: 'red', size: 18 }
+];
+
+var data4 = [
+    { color: 'blue' },
+    { color: 'green'},
+    { color: 'red'},
+    { color: 'blue'},
+    { color: 'orange'},
+    { color: 'purple'},
+    { color: 'rouge'},
+];
+var data4t1 = [
+    { color: 'blue' },
+    { color: 'blue'},
+    { color: 'green'},
+    { color: 'orange'},
+    { color: 'purple'},
+    { color: 'red'},
+    { color: 'rouge'},
+];
+
+var data5 = [
+    { color: 'blue' , size: 9 },
+    { color: 'green', size: 9 },
+    { color: 'red', size: 9 },
+    { color: 'blue', size: 9 },
+    { color: 'orange', size: 9 },
+    { color: 'purple', size: 9 },
+    { color: 'rouge', size: 9 },
+
+    { color: 'blue' , size: 15 },
+    { color: 'green', size: 16 },
+    { color: 'red', size: 18 },
+    { color: 'blue', size: 4 },
+    { color: 'orange', size: 11 },
+    { color: 'purple', size: 23 },
+    { color: 'rouge', size: 44 },    
+];
+var data5t1 = [
+    { color: 'blue' , size: 15 },
+    { color: 'green', size: 16 },
+    { color: 'orange', size: 11},
+    { color: 'purple', size: 23},
+    { color: 'red', size: 18},
+    { color: 'rouge', size: 44},
+];
+var data5t2 = [
+    { color: 'rouge', size: 44},
+    { color: 'red', size: 18},
+    { color: 'purple', size: 23},
+    { color: 'orange', size: 11},
+    { color: 'green', size: 16 },
+    { color: 'blue' , size: 15 },
+];
+var data5t3 = [
+    { color: 'purple', size: 23},
+    { color: 'orange', size: 11},
+    { color: 'green', size: 16 },
+];
+
+describe('#aquery(data).filter(\'size\', \'gt\', 10).run()', function () {
+    it('should return an array of items having the prop \'size\' greater than 10:', function () {
+        var result = aquery(data).filter('size', 'gt', 10).run();
+        assert.deepEqual(result, datat1);
+    });
+});
+
+describe('#aquery(data).filter(\'size\', \'gt\', 10).filter(\'color\', \'eq\', \'blue\').run()', function () {
+    it('should return an array of items having the prop \'size\' greater than 10 and color blue:', function () {
+        var result = aquery(data).filter('size', 'gt', 10).filter('color', 'eq', 'blue').run();
+        assert.deepEqual(result, datat2);
+    });
+});
+
+describe('#aquery(data).run()', function () {
+    it('should return original array:', function () {
+        var result = aquery(data).run();
+        assert.deepEqual(result, data);
+    });
+});
+
+describe('#aquery(data2).filter(null, \'gt\', 5).run()', function () {
+    it('should return an array of items having value greater than 5:', function () {
+        var result = aquery(data2).filter(null, 'gt', 5).run();
+        assert.deepEqual(result, data2t1);
+    });
+});
+
+describe('#aquery(data2).limit(10).run()', function () {
+    it('should return first 10 items of array:', function () {
+        var result = aquery(data2).limit(10).run();
+        assert.deepEqual(result, data2t2);
+    });
+});
+
+describe('#aquery(data2).offset(3).limit(10).run()', function () {
+    it('should return items 4 to 14 of array:', function () {
+        var result = aquery(data2).offset(3).limit(10).run();
+        assert.deepEqual(result, data2t3);
+    });
+});
+
+describe('#aquery(data3).filter(\'color\', \'contains\', \'bl\').run()', function () {
+    it('should return items with color prop containing \'bl\':', function () {
+        var result = aquery(data3).filter('color', 'contains', 'bl').run();
+        assert.deepEqual(result, data3t1);
+    });
+});
+
+describe('#aquery(data3).filter(\'color\', \'gt\', 5).run()', function () {
+    it('should return empty array as can\'t do gt than on string:', function () {
+        var result = aquery(data3).filter('color', 'gt', 5).run();
+        assert.deepEqual(result, []);
+    });
+});
+
+describe('#aquery(data4).orderBy(\'color\').run()', function () {
+    it('should return items ordered by color:', function () {
+        var result = aquery(data4).orderBy('color', 'desc', 'string').run();
+        assert.deepEqual(result, data4t1);
+    });
+});
+
+describe('#aquery(data5).filter(\'size\', \'gt\', 10).orderBy(\'color\', \'desc\', \'string\').run()', function () {
+    it('should return items ordered by color with size greater than 10:', function () {
+        var result = aquery(data5).filter('size', 'gt', 10).orderBy('color', 'desc', 'string').run();
+        assert.deepEqual(result, data5t1);
+    });
+});
+
+describe('#aquery(data5).filter(\'size\', \'gt\', 10).orderBy(\'color\', \'asc\', \'string\').run()', function () {
+    it('should return items ordered by color ASC with size greater than 10:', function () {
+        var result = aquery(data5).filter('size', 'gt', 10).orderBy('color', 'asc', 'string').run();
+        assert.deepEqual(result, data5t2);
+    });
+});
+
+describe('#aquery(data5).filter(\'size\', \'gt\', 10).orderBy(\'color\', \'asc\', \'string\').offset(2).limit(3).run()', function () {
+    it('should return items (offset 2 limit 3) ordered by color ASC with size greater than 10:', function () {
+        var result = aquery(data5).filter('size', 'gt', 10).orderBy('color', 'asc', 'string').offset(2).limit(3).run();
+        assert.deepEqual(result, data5t3);
+    });
+});
+
+describe('#aquery(data3).registerFilter({function})', function () {
+    it('should add a filter function to aquery:', function () {
+        var result = aquery().registerFilter('lengthof', function (a, b) {
+            if (a.length === b) {
+                return true;
+            }
+            return false;
+        });
+        assert.deepEqual(result, null);
+    });
+});
+
+describe('#aquery(data3).filter(\'color\', \'lengthof\', 3).run()', function () {
+    it('should return items with color props 3 chars long:', function () {
+        var result = aquery(data3).filter('color', 'lengthof', 3).run();
+        assert.deepEqual(result, data3t2);
+    });
+});
+
+describe('#filter_gt(a,b)', function () {
+    it('should not be accessable in global scope:', function () {
+        try {
+            var result = filter_gt(5, 7);
+        }
+        catch (err) {
+            assert.equal(true, true);
+        }
+    });
+});
